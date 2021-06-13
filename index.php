e1799b9bcdd390c3d16de9f810c12655332b0a15
araiyusuke <arai-t-ryousan06@tanita.co.jp>
2021-06-14 05:44:59 +0900

    index.phpを追加

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1>CodeDeployGitHubDemo Ver01</h>
+Your IP <?php echo $_SERVER['REMOTE_ADDR']?>
+</body>
+</html>
+~                                                                                                                                                          
+~               
